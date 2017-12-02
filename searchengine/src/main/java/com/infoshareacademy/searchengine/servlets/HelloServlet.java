0b0509e6cb26747a563a20879c7f036e6e2d6423
Pawel Gagala <pawelgagala@gmail.com>
2017-12-02 20:52:17 +0100

    remove redundant persistence.xml, turn on autoDetection

--- searchengine/src/main/java/com/infoshareacademy/searchengine/servlets/HelloServlet.java
+++ searchengine/src/main/java/com/infoshareacademy/searchengine/servlets/HelloServlet.java
@@ -1,5 +1,9 @@
 package com.infoshareacademy.searchengine.servlets;
 
+import com.infoshareacademy.searchengine.dao.UsersRepositoryDao;
+import com.infoshareacademy.searchengine.domain.User;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,8 +15,13 @@ import java.io.PrintWriter;
 @WebServlet("/hello-servlet")
 public class HelloServlet extends HttpServlet {
 
+
+    @EJB
+    private UsersRepositoryDao dao;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        dao.addUser(new User());
         PrintWriter writer = resp.getWriter();
         writer.println("Hello World!");
     }
